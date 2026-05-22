e3986f1703c813a98b90b6bec681cebda97b1d67
Voeun Savmeng <vsavmeng@kdsb.com.kh>
2026-05-22 13:59:29 +0700

    first commit

---
+++ src/main/java/com/example/swaggerdemo/SwaggerAPIController.java
@@ -0,0 +1,32 @@
+package com.example.swaggerdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class SwaggerAPIController {
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public List<String> getProducts() {
+        List<String> productsList = new ArrayList<>();
+        productsList.add("Honey");
+        productsList.add("Almond");
+        return productsList;
+    }
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public String createProduct() {
+        return "Product is saved successfully";
+    }
+    @RequestMapping(value = "/products",method = RequestMethod.DELETE)
+    public String deleteProduct(){
+        return "Product delete successfully";
+    }
+    @RequestMapping(value = "/products",method = RequestMethod.PUT)
+    public String updateProduct(){
+        return "Update product successfully";
+    }
+    
+}
